02219182767b7a6d3847d603582defe662233fca
stone141-star <katsunori612@gmail.com>
2024-11-09 16:16:00 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $Array = ['name' => 'orion', 'price' => '200', 'weight' => '160'];
+        print_r($Array);
+        ?>
+    </p>
+</body>
+</html>
