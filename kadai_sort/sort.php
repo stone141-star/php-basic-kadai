ab079a5f6b4ff93d4dd4b84e32a36e720587d921
stone141-star <katsunori612@gmail.com>
2024-11-15 11:14:34 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <title>PHP基礎編</title>
 </head>
-
 <body>
     <p>
         <?php
-        $nums = array(15, 4, 18, 23, 10 );
-        sort($nums);
-        foreach ($nums as $key => $val) {
-            
+        function sort_2way($order) {
+            $nums = array("15", "4", "18", "23", "10");
+
+            if ($order === TRUE) {
+                sort($nums);
+                echo "<br>昇順にソートします。<br>";
+            } else {
+                rsort($nums);
+                echo "<br>降順にソートします。<br>";
+            }
+
+            foreach($nums as $key => $val) {
+                echo "{$val}<br>";
+            }
         }
 
-        echo "<br>昇順にソートします。<br>{$nums[0]}<br>{$nums[1]}<br>{$nums[2]}<br>{$nums[3]}<br>{$nums[4]}";
-        $nums = array(15, 4, 18, 23, 10 );
-        rsort($nums);
-        foreach ($nums as $key => $val) {
-            
-        }
-
-        echo "<br>降順にソートします。<br>{$nums[0]}<br>{$nums[1]}<br>{$nums[2]}<br>{$nums[3]}<br>{$nums[4]}";
+        // 関数を呼び出す例
+        sort_2way(TRUE); // 昇順にソート
+        sort_2way(FALSE); // 降順にソート
         ?>
     </p>
 </body>
-
 </html>
